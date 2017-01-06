606b626deb31967fded2dc04b1a08aada481b068
nilskp <nilskp@gmail.com>
2017-01-06 17:51:52 -0600

    Forgot to override two other 'create index'

--- ulysses-jdbc/src/main/scala/ulysses/jdbc/mysql/MySQLDialect.scala
+++ ulysses-jdbc/src/main/scala/ulysses/jdbc/mysql/MySQLDialect.scala
@@ -6,18 +6,23 @@ import java.sql.Connection
 import java.sql.SQLException
 
 /**
- * MySQL dialect. Doesn't support schema.
- */
+  * MySQL dialect. Doesn't support schema.
+  */
 class MySQLDialect[ID: ColumnType, EVT, CH: ColumnType, SF: ColumnType]
     extends ulysses.jdbc.Dialect[ID, EVT, CH, SF](None) {
 
-  override def channelIndexDDL = super.channelIndexDDL.replace("IF NOT EXISTS ", "")
-  override def createChannelIndex(conn: Connection) = {
-    try {
-      super.createChannelIndex(conn)
-    } catch {
-      case sqlEx: SQLException if sqlEx.getErrorCode == 1061 => // Already exists
-    }
-  }
   override def metadataValType = "VARCHAR(32767)"
+
+  override def channelIndexDDL = super.channelIndexDDL.replace("IF NOT EXISTS ", "")
+  override def tickIndexDDL = super.channelIndexDDL.replace("IF NOT EXISTS ", "")
+  override def eventNameIndexDDL = super.eventNameIndexDDL.replace("IF NOT EXISTS ", "")
+
+  private def ignoreAlreadyExists(thunk: => Unit) = try thunk catch {
+    case sqlEx: SQLException if sqlEx.getErrorCode == 1061 => // Already exists, ignore
+  }
+
+  override def createChannelIndex(conn: Connection) = ignoreAlreadyExists(super.createChannelIndex(conn))
+  override def createTickIndex(conn: Connection) = ignoreAlreadyExists(super.createTickIndex(conn))
+  override def createEventNameIndex(conn: Connection) = ignoreAlreadyExists(super.createEventNameIndex(conn))
+
 }
