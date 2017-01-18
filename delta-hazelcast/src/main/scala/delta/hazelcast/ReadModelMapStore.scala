2a8070336507402a65398bf8e6c107be05dbec4b
nilskp <nilskp@gmail.com>
2017-01-17 19:17:45 -0600

    Use generic ReadModelStore as backend.

--- delta-hazelcast/src/main/scala/delta/hazelcast/ReadModelMapStore.scala
+++ delta-hazelcast/src/main/scala/delta/hazelcast/ReadModelMapStore.scala
@@ -1,25 +1,33 @@
 package delta.hazelcast
 
 import collection.JavaConverters._
-import collection.Map
 
 import com.hazelcast.core.MapStore
+import delta.cqrs.ReadModelStore
 
-import java.util.{
-  Collection,
-  Map => JMap
-}
+import concurrent.duration._
 
-trait ReadModelMapStore[K, D]
+import scuff.concurrent._
+
+import java.util.{ Collection, Map => JMap }
+import com.hazelcast.logging.ILogger
+
+class ReadModelMapStore[K, D](
+  store: ReadModelStore[K, D])(
+    implicit logger: ILogger)
     extends MapStore[K, ReadModelState[D, Any]] {
 
-  def delete(k: K) = ()
-  def deleteAll(keys: Collection[K]) = ()
+  protected def DefaultTimeout = 33.seconds
+
+  def loadAllKeys = null
+
+  def delete(k: K) = logger.warning(s"Tried to delete $k, but ignoring. Override method if needed.")
+  def deleteAll(keys: Collection[K]) = logger.warning(s"Tried to delete ${keys.size} keys, but ignoring. Override method if needed.")
 
   def store(key: K, state: ReadModelState[D, Any]): Unit = {
     if (state.unapplied.isEmpty) {
-      if (state.dataUpdated) storeModel(key, state.model)
-      else updateRevision(key, state.model.revision, state.model.tick)
+      if (state.dataUpdated) store.set(key, state.model).await(DefaultTimeout)
+      else store.update(key, state.model.revision, state.model.tick).await(DefaultTimeout)
     }
   }
   def storeAll(map: JMap[K, ReadModelState[D, Any]]) = {
@@ -29,24 +37,17 @@ trait ReadModelMapStore[K, D]
     val revUpdated = map.asScala.collect {
       case (key, ReadModelState(model, false, unapplied)) if unapplied.isEmpty => key -> (model.revision, model.tick)
     }
-    if (dataUpdated.nonEmpty) storeModels(dataUpdated)
-    if (revUpdated.nonEmpty) updateRevisions(revUpdated)
+    if (dataUpdated.nonEmpty) store.setAll(dataUpdated).await(DefaultTimeout)
+    if (revUpdated.nonEmpty) store.updateAll(revUpdated).await(DefaultTimeout)
   }
 
-  def load(key: K): ReadModelState[D, Any] = loadModel(key) match {
+  def load(key: K): ReadModelState[D, Any] = store.get(key).await(DefaultTimeout) match {
     case Some(model) => new ReadModelState(model)
     case _ => null
   }
   def loadAll(keys: Collection[K]) = {
-    val models = loadModels(keys.asScala)
+    val models = store.getAll(keys.asScala).await(DefaultTimeout)
     models.mapValues(m => new ReadModelState[D, Any](m)).asJava
   }
-
-  def loadModel(key: K): Option[ReadModel[D]]
-  def storeModel(key: K, model: ReadModel[D]): Unit
-  def updateRevision(key: K, revision: Int, tick: Long)
-  def loadModels(keys: Iterable[K]): Map[K, ReadModel[D]]
-  def storeModels(model: Map[K, ReadModel[D]]): Unit
-  def updateRevisions(revisions: Map[K, (Int, Long)]): Unit
 
 }
