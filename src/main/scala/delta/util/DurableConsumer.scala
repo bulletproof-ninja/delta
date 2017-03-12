f38e8a8fc2be351ab7550064b3f4cc61ca07440e
nilskp <nilskp@gmail.com>
2017-03-11 19:01:41 -0600

    Refactor and simplify.

--- src/main/scala/delta/util/DurableConsumer.scala
+++ src/main/scala/delta/util/DurableConsumer.scala
@@ -1,87 +1,115 @@
 package delta.util
 
-import java.util.concurrent.{ CountDownLatch, TimeoutException }
-import java.util.concurrent.atomic.AtomicReference
-
 import scala.concurrent.{ ExecutionContext, Future }
-import scala.concurrent.duration.DurationInt
 
-import scuff.Subscription
-import scuff.concurrent.{ StreamCallback, StreamPromise }
 import delta.{ EventSource, Transaction }
+import scuff.Subscription
+import scuff.concurrent.StreamPromise
 
 trait DurableConsumer[ID, EVT, CH] {
 
   type TXN = Transaction[ID, EVT, CH]
   type ES = EventSource[ID, EVT, CH]
 
-  protected def liveSwitchProcessingTimeout = 33.seconds
-  protected def selector[T <: ES](es: T): T#Selector
-  protected def newProcessor(es: ES): TwoPhaseProcessor[TXN]
+  /** Transaction selector. */
+  protected def selector[T <: ES](es: T): es.Selector
+  /** Last processed tick, if exists. */
+  protected def lastProcessedTick: Future[Option[Long]]
+  /**
+    * Called when historic processing begins.
+    * This is distinct from live processing and
+    * allows the following optimizations:
+    *     1) Predictable monotonic increasing revisions
+    *     2) No duplicate revisions
+    *     3) Potential for in-memory processing
+    * NOTE: If the event source is empty, this will
+    * not be called.
+    */
+  protected def getHistoricProcessor(): TXN => Unit
+  /**
+    * When caught up on historic transactions,
+    * a live processor is requested. Any in-memory
+    * processing from historic processing can be
+    * persisted when called, before returning the
+    * processing function.
+    * A live processor must take care to handle these
+    * conditions:
+    *     1) Out-of-order revisions
+    *     2) Duplicate (repeated) revisions
+    *     3) Concurrent calls
+    */
+  protected def getLiveProcessor(): Future[TXN => Unit]
 
   /**
-    * Resume consumption.
+    * Start processing of transactions, either from beginning
+    * of time, or from where left off when last ended.
     * @param es The [[EventSource]] to process.
     * @param maxTickSkew The anticipated max tick skew.
+    * @return A future subscription to live events.
+    * This will be available when historic processing is done,
+    * thus state is considered current.
     */
-  def resume(es: ES, maxTickSkew: Int)(implicit ec: ExecutionContext): Future[Subscription] = {
+  def startProcessing(es: ES, maxTickSkew: Int)(implicit ec: ExecutionContext): Future[Subscription] = {
     require(maxTickSkew >= 0, s"Cannot have negative tick skew: $maxTickSkew")
-    es.lastTick.flatMap { lastTickAtStart =>
-      val processor = newProcessor(es)
-      val catchUpQuery = processor.lastProcessedTick match {
-        case None =>
-          es.query(selector(es)) _
-        case Some(lastSeen) =>
-          es.querySince(lastSeen - maxTickSkew, selector(es)) _
-      }
-      val catchUpFuture = StreamPromise.foreach(catchUpQuery)(processor.historic)
-      catchUpFuture.map { _ =>
-        val switcher = {
-          new SafeCatchUpSwitcher(processor.live)
+    this.lastProcessedTick.flatMap {
+      case None =>
+        es.lastTick().flatMap { maybeLastTick =>
+          start(es, maybeLastTick)(selector(es), maxTickSkew)
         }
-        val liveSubscription = es.subscribe(selector(es))(switcher.live)
-        lastTickAtStart match {
+      case Some(lastProcessed) =>
+        es.lastTick().flatMap {
+          case Some(lastTick) =>
+            resume(es, lastTick)(selector(es), lastProcessed, maxTickSkew)
           case None =>
-            es.query(selector(es))(switcher.callback(ec.reportFailure))
-          case Some(lastTickAtStart) =>
-            es.querySince(lastTickAtStart - maxTickSkew, selector(es))(switcher.callback(ec.reportFailure))
+            throw new IllegalStateException(s"Last processed tick is $lastProcessed, but event source is empty: $es")
         }
-
-        liveSubscription
-      }
     }
   }
 
-  private final class SafeCatchUpSwitcher(process: TXN => Unit) {
-    private[this] val state = new AtomicReference[Either[List[TXN], CountDownLatch]](Left(Nil))
-    def live(txn: TXN): Unit = state.get match {
-      case null => process(txn)
-      case ll @ Left(list) =>
-        if (!state.compareAndSet(ll, Left(txn :: list))) {
-          live(txn) // Retry
-        }
-      case Right(latch) =>
-        val latchTimeout = liveSwitchProcessingTimeout
-        if (latch.await(latchTimeout.length, latchTimeout.unit)) {
-          state.set(null)
-          process(txn)
-        } else {
-          throw new TimeoutException(s"Timed out after $latchTimeout, awaiting catch-up processing")
-        }
+  private def start(es: ES, lastTickAtStart: Option[Long])(
+    selector: es.Selector, maxTickSkew: Int)(
+      implicit ec: ExecutionContext): Future[Subscription] = {
+    val historicProcessingDone: Future[Unit] = lastTickAtStart match {
+      case None => // EventSource is empty
+        Future successful None
+      case Some(_) =>
+        val historicQuery = es.query(selector) _
+        val historicProcessor = this.getHistoricProcessor()
+        StreamPromise.foreach(historicQuery)(historicProcessor)
     }
-    def callback(reportFailure: Throwable => Unit) = new StreamCallback[TXN] {
-      def onNext(txn: TXN): Unit = process(txn)
-      def onError(th: Throwable): Unit = reportFailure(th)
-      def onCompleted(): Unit = {
-        val ll = state.get
-        val list = ll.left.get
-        val latch = new CountDownLatch(1)
-        if (state.compareAndSet(ll, Right(latch))) {
-          list.reverseIterator.foreach(process)
-          latch.countDown()
-        } else onCompleted() // Retry
+    historicProcessingDone.flatMap { _ =>
+      this.getLiveProcessor().flatMap { liveProcessor =>
+        val liveSubscription = es.subscribe(selector.toMonotonic)(liveProcessor)
+        // close window of opportunity, for a potential race condition, by re-querying anything since start
+        val windowClosed = lastTickAtStart match {
+          case None =>
+            val windowQuery = es.query(selector) _
+            StreamPromise.foreach(windowQuery)(liveProcessor)
+          case Some(lastTickAtStart) =>
+            val windowQuery = es.querySince(lastTickAtStart - maxTickSkew, selector) _
+            StreamPromise.foreach(windowQuery)(liveProcessor)
+        }
+        windowClosed.map(_ => liveSubscription)
       }
-
+    }
+  }
+  private def resume(es: ES, lastTickAtStart: Long)(
+    selector: es.Selector, lastProcessedTick: Long, maxTickSkew: Int)(
+      implicit ec: ExecutionContext): Future[Subscription] = {
+    val historicProcessingDone: Future[Unit] = {
+      val historicQuery = es.querySince(lastProcessedTick - maxTickSkew, selector) _
+      val historicProcessor = this.getHistoricProcessor()
+      StreamPromise.foreach(historicQuery)(historicProcessor)
+    }
+    historicProcessingDone.flatMap { _ =>
+      this.getLiveProcessor().flatMap { liveProcessor =>
+        val liveSubscription = es.subscribe(selector.toMonotonic)(liveProcessor)
+        // close window of opportunity, for a potential race condition, by re-querying anything since start
+        val windowQuery = es.querySince(lastTickAtStart - maxTickSkew, selector) _
+        StreamPromise
+          .foreach(windowQuery)(liveProcessor)
+          .map(_ => liveSubscription)
+      }
     }
   }
 
