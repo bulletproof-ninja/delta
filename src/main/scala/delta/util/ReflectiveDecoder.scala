dd9ecf46f46d5e9fa1569bcbee33caba16b05881
nilskp <nilskp@gmail.com>
2017-08-31 12:31:05 -0500

    Refactor ReflectiveDecoder
    Add decoder validation against encoder methods, if they exist.
    Allow using decoder method name as event name, instead of return type, to allow for methods of deprecated events that are upgraded to new events.

--- src/main/scala/delta/util/ReflectiveDecoder.scala
+++ src/main/scala/delta/util/ReflectiveDecoder.scala
@@ -1,5 +1,6 @@
 package delta.util
 
+import scuff._
 import scala.reflect.{ ClassTag, classTag }
 import java.lang.reflect.Method
 import delta.EventCodec
@@ -11,36 +12,68 @@ import delta.NoVersioning
   * the following signature:
   * For versioned events (default), the method must have two
   * arguments, 1. `Byte`, 2. `SF` (the encoding format) and
-  * must return an EVT type.
-  * For `NoVersioning` events, the method must take a single
-  * argument, `SF`, and must return a type of `EVT`.
+  * for `NoVersioning` events, the method must take a single
+  * argument, `SF`.
   */
 abstract class ReflectiveDecoder[EVT: ClassTag, SF <: AnyRef: ClassTag] {
-  evtCtx: EventCodec[EVT, SF] =>
+  codec: EventCodec[EVT, SF] =>
+
+  /**
+    * Is method name == event name?
+    *
+    * If `true` then it's possible to have multiple
+    * methods decode to the same event. This is helpful
+    * when using deprecated events that are upgraded to
+    * new event type.
+    * If `false` then method name is ignored, but return
+    * type defines which event the method decodes.
+    */
+  protected def isMethodNameEventName: Boolean = false
+  private def eventName(method: Method): String =
+    if (isMethodNameEventName) method.getName
+    else codec.name(method.getReturnType.asInstanceOf[Class[EVT]])
 
   private[this] lazy val decoderMethods: Map[String, Method] = {
-    val noVersion = evtCtx.isInstanceOf[NoVersioning[_, _]]
-    val parmCount = if (noVersion) 1 else 2
+    val noVersion = codec.isInstanceOf[NoVersioning[_, _]]
+    val argCount = if (noVersion) 1 else 2
     val ByteClass = classOf[Byte]
     val EvtClass = classTag[EVT].runtimeClass
     val FmtClass = classTag[SF].runtimeClass
-    val methods = getClass.getMethods.filter { m =>
-      val parms = m.getParameterTypes
-      parms.length == parmCount &&
-        parms(parmCount - 1).isAssignableFrom(FmtClass) &&
-        (noVersion || parms(0) == ByteClass) &&
+    val decoderMethods = getClass.getMethods.filter { m =>
+      val argTypes = m.getParameterTypes
+      argTypes.length == argCount &&
+        argTypes(argCount - 1).isAssignableFrom(FmtClass) &&
+        (noVersion || argTypes(0) == ByteClass) &&
         EvtClass.isAssignableFrom(m.getReturnType)
-    }.map(m => evtCtx.name(m.getReturnType.asInstanceOf[Class[EVT]]) -> m)
-    methods.groupBy(_._1).toSeq.filter(_._2.size > 1).headOption.foreach {
-      case (evtName, methods) =>
-        val methodsString = methods.mkString(Platform.EOL, Platform.EOL, "")
-        throw new IllegalStateException(
-          s"""Event "$evtName" has ambiguous decoding by the following methods:$methodsString""")
+    }.map(m => eventName(m) -> m)
+    // When using return type, there can be more than one decoder. Verify there's not.
+    if (!isMethodNameEventName) {
+      decoderMethods.groupBy(_._1).toSeq.filter(_._2.size > 1).headOption.foreach {
+        case (evtName, methods) =>
+          val nlIndent = Platform.EOL + "\t"
+          val methodsString = methods.mkString(nlIndent, nlIndent, "")
+          throw new IllegalStateException(
+            s"""Event "$evtName" has ambiguous decoding by the following methods:$methodsString""")
+      }
     }
-    if (methods.isEmpty) {
-      throw new IllegalStateException(s"No decoding methods found for $getClass")
+    val encoderEvents = getClass.getMethods.flatMap { m =>
+      val argTypes = m.getParameterTypes
+      if (argTypes.length == 1 &&
+        EvtClass.isAssignableFrom(argTypes(0)) &&
+        EvtClass != argTypes(0) &&
+        FmtClass.isAssignableFrom(m.getReturnType)) {
+        val evtType = argTypes(0).asInstanceOf[Class[EVT]]
+        Some(codec.name(evtType))
+      } else None
+    }.toSet
+    val decoderMethodsByName = decoderMethods.toMap
+    assert(decoderMethodsByName.size == decoderMethods.size)
+    val missingDecoders = encoderEvents.filterNot(decoderMethodsByName.contains)
+    if (missingDecoders.nonEmpty) {
+      val missing = missingDecoders.mkString("[", ", ", "]")
+      throw new IllegalStateException(s"No decoder methods found in ${getClass} for events $missing")
     }
-    methods.toMap
+    decoderMethodsByName
   }
 
   private def decoderNotFound(evtName: String, noVersion: Boolean): Nothing = {
