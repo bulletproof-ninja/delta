a7d3675b50cf6a85d15c1c20aab1fdacde09e8c5
Nils Kilden-Pedersen <nilskp@gmail.com>
2018-06-15 08:40:53 -0500

    Factor out Subscriber class

--- src/main/scala/delta/util/NotificationSupport.scala
+++ src/main/scala/delta/util/NotificationSupport.scala
@@ -1,80 +1,91 @@
 package delta.util
 
-import scala.concurrent.ExecutionContext
-import scuff.concurrent.MultiMap
-import scala.concurrent.Future
+import scala.collection.concurrent.TrieMap
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Failure
+
 import scuff.Subscription
+import scuff.concurrent.MultiMap
 
 /**
   * @tparam ID The id type
   * @tparam S The state type
-  * @tparam FMT The Snapshot notification type
+  * @tparam PF The Snapshot publish format
   */
-trait NotificationSupport[ID, S >: Null, FMT] {
+trait NotificationSupport[ID, S >: Null, PF] {
   consumer: DefaultEventSourceConsumer[ID, _, S] =>
 
-  private class Subscriber(val id: ID, thunk: PartialFunction[(ID, Snapshot, FMT), Unit], notificationCtx: ExecutionContext) {
-    // Best effort to avoid race condition, while not incurring unnecessary cost
-    @volatile private[this] var blind: Boolean = true
-    def onUpdate(snapshot: Snapshot, formatted: FMT): Unit = {
-      val input = (id, snapshot, formatted)
-      if (thunk isDefinedAt input) {
-        blind = false
-        notificationCtx execute new Runnable {
-          def run = thunk(input)
-          override def hashCode = id.##
-        }
-      }
-    }
-    def onLatest(snapshot: Snapshot, formatted: FMT): Unit =
-      if (blind) {
-        val input = (id, snapshot, formatted)
-        notificationCtx execute new Runnable {
-          def run = if (blind) thunk(input)
-          override def hashCode = id.##
-        }
-      }
-  }
+  private type Subscriber = NotificationSubscriber[ID, S, PF, Callback]
+  private[this] val subscribers = new TrieMap[FormatKey, MultiMap[ID, Subscriber]]
 
-  private[this] val subscribers = new MultiMap[ID, Subscriber]
-
+  /**
+    * Publish format key type.
+    */
+  protected type FormatKey
   protected def contentUpdatesOnly: Boolean = false
-  protected def format(id: ID, snapshot: Snapshot, contentUpdated: Boolean): FMT
+  protected def format(key: FormatKey, id: ID, snapshot: Snapshot, contentUpdated: Boolean): PF
   protected def store: StreamProcessStore[ID, S]
 
   protected def onSnapshotUpdate(id: ID, snapshot: Snapshot, contentUpdated: Boolean): Unit = {
     if (contentUpdated || !contentUpdatesOnly) {
-      val subs = subscribers(id)
-      if (subs.nonEmpty) {
-        val formatted = format(id, snapshot, contentUpdated)
-        subs.foreach(_.onUpdate(snapshot, formatted))
+      subscribers.foreach {
+        case (fmtKey, subscribers) =>
+          val subs = subscribers(id)
+          if (subs.nonEmpty) {
+            val formatted = format(fmtKey, id, snapshot, contentUpdated)
+            subs.foreach(_.onUpdate((id, snapshot, formatted)))
+          }
       }
+    }
+  }
+
+  type Callback = (ID, Snapshot, PF)
+
+  private def unsubscribe(fmtKey: FormatKey, subs: Map[ID, Subscriber]): Unit = {
+    val subscribers = this.subscribers(fmtKey)
+    subs.foreach {
+      case (id, sub) => subscribers(id) -= sub
     }
   }
 
   /**
     * Subscribe to latest state.
-    * NOTE: If id exists, subscriber will always get the
+    * NOTE: If id currently exists,
+    * subscriber will always get the
     * latest state automatically.
     */
-  protected def subscribe(notificationCtx: ExecutionContext)(ids: Iterable[ID])(thunk: PartialFunction[(ID, Snapshot, FMT), Unit]): Future[Subscription] = {
+  protected def subscribe(notificationCtx: ExecutionContext)(ids: Iterable[ID], fmtKey: FormatKey)(callback: PartialFunction[Callback, Unit]): Future[Subscription] = {
     require(ids.nonEmpty, "Must have at least one id to subscribe")
-
-    val subs = ids.map { id =>
-      val sub = new Subscriber(id, thunk, notificationCtx)
+    val subscribers = this.subscribers.get(fmtKey) match {
+      case Some(subscribers) =>
+        subscribers
+      case None =>
+        val multimap = new MultiMap[ID, Subscriber]
+        this.subscribers.putIfAbsent(fmtKey, multimap) getOrElse multimap
+    }
+    val subById = ids.map { id =>
+      val sub = new Subscriber(id, notificationCtx)(callback)
       subscribers(id) += sub
       id -> sub
     }.toMap
-    store.readBatch(subs.keys).map { result =>
-      result.foreach {
-        case (id, latest) => subs(id).onLatest(latest, format(id, latest, true))
-      }
-      new Subscription {
-        def cancel = subs.foreach {
-          case (id, sub) => subscribers(id) -= sub
+
+      implicit def ec = notificationCtx
+
+    store.readBatch(ids)
+      .andThen {
+        case Failure(_) => unsubscribe(fmtKey, subById)
+      } map { result =>
+        subById.foreach {
+          case (id, sub) =>
+            val initialState = result.get(id).map { snapshot =>
+              (id, snapshot, format(fmtKey, id, snapshot, true))
+            }
+            sub.onInitial(initialState)
+        }
+        new Subscription {
+          def cancel = unsubscribe(fmtKey, subById)
         }
       }
-    }(notificationCtx)
 
   }
 }
